7d621a4498c1f394473c78da577461894521342a
anil935 <94883184+anil935@users.noreply.github.com>
2022-06-06 04:29:25 +0000

    string

--- ActivitySet01/problem02.py
+++ ActivitySet01/problem02.py
@@ -1,3 +1,22 @@
-# Why Program
+str1="hello"
+str2='there'
+bob=str1+str2
+print(bob)
+str3="123"
+bob=int(str3)+1
+print(bob)
+print(int)
+str4='jat'''
+str="banana"
+x=3
+print(str[x])
+print(str[x-1])
+print(len(str))
+...
+name='reva'
+i=0
+while i<len(name):
+ letter = name[i]
+ print(i,letter)
+ i=i+1
 
-prinq("hello world")
